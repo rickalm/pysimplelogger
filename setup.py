bbd9e5b96d5ff0c0025f17cb5fadea5c321d536b
Rick Alm <rickalm@users.noreply.github.com>
2019-02-12 16:56:25 -0600

    missing comma

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pysimplelogger',
       version='0.1',
-      description='Simple Logger based on python logging'
+      description='Simple Logger based on python logging',
       url='http://github.com/rickalm/pysimplelogger',
       author='Rick Alm',
       author_email='rickealm@gmail.com',
